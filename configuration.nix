4d42cafaa25c33e8f89a7b2285784c114f0c7a67
MayNiklas <info@niklas-steffen.de>
2024-08-19 02:24:43 +0200

    add formatter

--- configuration.nix
+++ configuration.nix
@@ -2,17 +2,18 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{inputs, config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 let
-	user = "matthiasw";
+  user = "matthiasw";
 in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      
+
     ];
- 
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -50,7 +51,7 @@ in
 
   # Enable the GNOME Desktop Environment.
   services.xserver.desktopManager.plasma5.enable = true;
-  services.displayManager.sddm.enable  = true;
+  services.displayManager.sddm.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -86,12 +87,12 @@ in
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner"];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner" ];
     packages = with pkgs; [
-   	#thunderbird
+      #thunderbird
     ];
   };
-	
+
 
 
   # Enable automatic login for the user.
@@ -151,12 +152,12 @@ in
   #enables flakes and experimental features
   #weekly garbage collector to remove old stores
   nix = {
-	settings = { 
-		auto-optimise-store = true;
-		allowed-users = ["matthiasw"];
-		experimental-features = [ "nix-command" "flakes" ];
-	};
-	gc = {automatic = true; dates = "weekly"; options = "--delete-older-than 30d";};
+    settings = {
+      auto-optimise-store = true;
+      allowed-users = [ "matthiasw" ];
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+    gc = { automatic = true; dates = "weekly"; options = "--delete-older-than 30d"; };
   };
 
 }
--- flake.nix
+++ flake.nix
@@ -3,50 +3,56 @@
   description = "A very basic flake";
   #unstable?
   inputs = {
- 	nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-   	#home-manager = {
-	#	url = "github:nix-community/home-manager";
-	#	inputs.nixpkgs.follows = "nixpkgs";
-    	#};
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    #home-manager = {
+    #	url = "github:nix-community/home-manager";
+    #	inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
-	 
-  outputs = { self, nixpkgs, ... }@inputs: 
-	let
-	system = "x86_64-linux";
-	username = "matthiasw";
-	lib = nixpkgs.lib;
-	pkgs = import nixpkgs {
- 		inherit system;
-		config.allowUnfree = true;
-	}; 
 
-	in{
+  outputs = { self, ... }@inputs:
+    with inputs;
+    let
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
+      system = "x86_64-linux";
+      username = "matthiasw";
+      lib = nixpkgs.lib;
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
 
+      # Use nixpkgs-fmt for `nix fmt'
+      formatter = forAllSystems
+        (system: nixpkgsFor.${system}.nixpkgs-fmt);
 
-	nixosConfigurations = {
-		matthiasw = lib.nixosSystem {
-	    		specialArgs = {inherit inputs system; };
-	   		modules = [ 
-				./configuration.nix 
-	   		];
-	 	};
-	};
+      nixosConfigurations = {
+        matthiasw = lib.nixosSystem {
+          specialArgs = { inherit inputs system; };
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
 
-
-	#hmConfig = {
-	#matthiasw = home-manager.lib.homeManagerConfiguration {
-	#	pkgs = nixpkgs.legacyPackages.${system};
-	#	modules = [
-	#		./home.nix
-	#	{
-	#	home = {
-	#		username = "matthiasw";
-	#		homeDirectory = "/home/matthiasw";
-	#		stateVersion = "24.05";
-	#	};			
-	#	}
-	#	];
-	#};
-	#};
-};
+      # hmConfig = {
+      #   matthiasw = home-manager.lib.homeManagerConfiguration {
+      #     pkgs = nixpkgs.legacyPackages.${system};
+      #     modules = [
+      #       ./home.nix
+      #       {
+      #         home = {
+      #           username = "matthiasw";
+      #           homeDirectory = "/home/matthiasw";
+      #           stateVersion = "24.05";
+      #         };
+      #       }
+      #     ];
+      #   };
+      # };
+    };
 }
